b2b2daa1b1d7027eb5c0609b76ec94a094585ce1
Usuario <Usuario@CPD01-M11>
2024-10-03 15:37:21 -0300

    Adicionar arquivos de projeto.

---
+++ MauiGuiaCurso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGuiaCurso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
