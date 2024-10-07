b0ee906e79a9cf2a91aee1ead2390d1f4e59daeb
Usuario <Usuario@CPD03-M14>
2024-10-07 16:00:32 -0300

    Início da criação da tela inicial.

--- MauiGuiaCurso/MainPage.xaml.cs
+++ MauiGuiaCurso/MainPage.xaml.cs
@@ -1,7 +1,43 @@
-﻿namespace MauiGuiaCurso
+﻿using MauiGuiaCurso.Anos;
+
+namespace MauiGuiaCurso
 {
     public partial class MainPage : ContentPage
     {
-        
+        private void primeiro_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Primeiro());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("OPS!!", ex.Message, "OK");
+            }
+        }
+
+        private void segundo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Segundo());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("OPS!!", ex.Message, "OK");
+            }
+        }
+
+        private void terceiro_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Terceiro());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("OPS!!", ex.Message, "OK");
+            }
+        }
     }
 }
